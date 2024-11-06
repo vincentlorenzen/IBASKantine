150f0fa887b38504287ad9619e0f177b7e0e8d6c
vincentlorenzen <vincent.lorenzen@gmail.com>
2024-11-06 11:04:42 +0100

    Added ContactInfo RAzor page

---
+++ Pages/ContactInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ibas.MenuApp.Pages
+{
+    public class ContactInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
